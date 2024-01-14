dea5cdef76d7e5095e7514a15f9a7ab3ecd8278e
Evgenia Iohina <evgeniaiohina@yandex.ru>
2024-01-15 01:27:37 +0200

    add TodoTask and save in LocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React, { ChangeEvent, FC, useState } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
+import TodoTask from './components/TodoTask';
+import { log } from 'console';
 
 const App: FC = () => {
+  let loadedTodoList = JSON.parse(localStorage.getItem("todos")!);
+
   const [task, setTask] = useState<string>("");
-  const [todoList, setTodo] = useState<Array<string>>([]);
+  const [todoList, setTodo] = useState<Array<string>>(loadedTodoList);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setTask(event.target.value);
@@ -15,12 +19,35 @@ const App: FC = () => {
     setTask("");
     console.log(todoList)
   }
+
+  const completeTask = (taskDelete: string): void => {
+    setTodo(todoList.filter((task)=> {
+      return task !== taskDelete
+    } ))
+  }
+
+  // React.useEffect(() => {
+  //   let todos = [];
+  //   const temp = localStorage.getItem("todos")!;
+  //   if(temp) {
+  //     todos = (temp);
+  //   }
+  // })
+  React.useEffect(()=>{
+    const temp = JSON.stringify(todoList)
+    localStorage.setItem("todos", temp);
+
+  }, [todoList])
+
     return (
         <div className="App">
             <p className='header'>Todo-List-App</p>
             <TodoItem onHandleChange={handleChange} onClick={addTask} valueInput={task}/>
-
-            <div className="todoList"></div>
+            <div className="todoList">
+              {todoList.map((task: string, key: number) => {
+                return<TodoTask key={key} task={task} remove={completeTask}/>
+              })}
+            </div>
         </div>
     );
 };
--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, {FC} from "react";
+import './todoTaskStyles.css';
 
-const TodoTask = () => {
-return <></>
+
+const trash = require ('../images/trash.png');
+
+type Props = {
+    task: string;
+    remove(taskDelete:string): void;
+}
+const TodoTask: FC<Props> = ({task, remove}:Props) => {
+return (
+    <>
+    <div className="task">
+        <div className="content">
+            <span>{task}</span>
+        </div>
+        <button className="btn-remove" onClick={() => {
+            remove(task);
+        }}>
+            <img src={trash}/>
+        </button>
+    </div>
+    </>
+)
+
 }
 
 export default TodoTask;
